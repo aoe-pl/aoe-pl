b74324141befcf9ad2a9396dce0f29a9ba6e1725
pawel1894 <pawepohl@gmail.com>
2025-05-06 19:08:21 +0200

    adding icons

---
+++ src/components/icons/index.ts
@@ -0,0 +1,6 @@
+export { CogIcon } from "./cog";
+export { HomeIcon } from "./home";
+export { NewsIcon } from "./news";
+export { TrophyIcon } from "./trophy";
+export { UsersIcon } from "./users";
+export { BackspaceIcon } from "./backspace";
