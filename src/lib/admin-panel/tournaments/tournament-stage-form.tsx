d334d674a208ce4191dbfe4bcec77a4cb89d06c3
pawel1894 <pawepohl@gmail.com>
2025-06-15 18:42:54 +0200

    remove unused imports

--- src/lib/admin-panel/tournaments/tournament-stage-form.tsx
+++ src/lib/admin-panel/tournaments/tournament-stage-form.tsx
@@ -26,8 +26,6 @@ import { ScrollArea } from "@/components/ui/scroll-area";
 import {
   BracketType,
   bracketTypesLabels,
-  stageTypesLabels,
-  TournamentStageType,
   type TournamentStage,
   type TournamentStageFormSchema,
 } from "./tournament";
