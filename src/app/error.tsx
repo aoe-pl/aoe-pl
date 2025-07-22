15ee372ab155ae51857d9758e7d1761fe218109c
pawel1894 <pawepohl@gmail.com>
2025-07-22 22:27:09 +0200

    fix: error fallback logic

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import Link from "next/link";
+import { useSession } from "next-auth/react";
 
 export default function Error({
   error,
@@ -10,17 +11,17 @@ export default function Error({
   reset: () => void;
 }) {
   // Check if it's a TRPC UNAUTHORIZED error
-  const isUnauthorized = error.message.includes("UNAUTHORIZED");
+  const session = useSession();
 
   return (
     <div className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
       <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16">
         <h2 className="text-2xl font-bold">
-          {isUnauthorized
-            ? "You don't have permission to access this content"
-            : "Something went wrong!"}
+          {session.status === "unauthenticated" &&
+            "You don't have permission to access this content"}
         </h2>
-        {isUnauthorized ? (
+        <span>{error.message}</span>
+        {session.status === "unauthenticated" ? (
           <Link
             href="/api/auth/signin"
             className="rounded-full bg-white/10 px-10 py-3 font-semibold no-underline transition hover:bg-white/20"
