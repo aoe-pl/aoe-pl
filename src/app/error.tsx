967384f4e2d6d7267753860290b7dec966b33eb5
pawel1894 <pawepohl@gmail.com>
2025-07-22 22:39:47 +0200

    Revert "fix: error fallback logic"
    This reverts commit 15ee372ab155ae51857d9758e7d1761fe218109c.

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import Link from "next/link";
-import { useSession } from "next-auth/react";
 
 export default function Error({
   error,
@@ -11,31 +10,28 @@ export default function Error({
   reset: () => void;
 }) {
   // Check if it's a TRPC UNAUTHORIZED error
-  const session = useSession();
+  const isUnauthorized = error.message.includes("UNAUTHORIZED");
 
   return (
     <div className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
       <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16">
         <h2 className="text-2xl font-bold">
-          {session.status === "unauthenticated" &&
-            "You don't have permission to access this content"}
+          {isUnauthorized
+            ? "You don't have permission to access this content"
+            : "Something went wrong!"}
         </h2>
-        <span>{error.message}</span>
-        {session.status === "unauthenticated" ? (
-          <Link
-            href="/api/auth/signin"
-            className="rounded-full bg-white/10 px-10 py-3 font-semibold no-underline transition hover:bg-white/20"
-          >
-            Sign in to continue
-          </Link>
-        ) : (
-          <button
-            onClick={() => reset()}
-            className="rounded-full bg-white/10 px-10 py-3 font-semibold no-underline transition hover:bg-white/20"
-          >
-            Try again
-          </button>
-        )}
+        <Link
+          href="/api/auth/signin"
+          className="rounded-full bg-white/10 px-10 py-3 font-semibold no-underline transition hover:bg-white/20"
+        >
+          Sign in to continue
+        </Link>
+        <button
+          onClick={() => reset()}
+          className="rounded-full bg-white/10 px-10 py-3 font-semibold no-underline transition hover:bg-white/20"
+        >
+          Try again
+        </button>
       </div>
     </div>
   );
